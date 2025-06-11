a1a98a9486adb613b3ee454572e4c2d29f9e5e63
xcodegou <xcodegou@tencent.com>
2025-06-11 15:16:47 +0800

    init SPM
    Signed-off-by: xcodegou <xcodegou@tencent.com>

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "SocketRocket",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "SocketRocket",
+            targets: ["SocketRocket"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "SocketRocket",
+            dependencies: [],
+            path: "SocketRocket",
+            sources: [".", "../SocketRocket_Internal"],
+            publicHeadersPath: "."
+        )
+    ]
+) 
